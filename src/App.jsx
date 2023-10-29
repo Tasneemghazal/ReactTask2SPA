0f3ffb15f615fe5e68c448350ac770558502d8e2
Tasneem <tasneemgazal5@gmail.com>
2023-10-29 18:19:45 +0200

    import Home.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navbar from './components/navbar/Navbar.jsx'
 import Products from './components/products/Products.jsx'
 import About from './components/about/About.jsx'
 import PageNotFound from './components/pageNotFound/PageNotFound.jsx'
+import Home from './components/home/Home.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path='/products' element={<Products/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/*' element={<PageNotFound/>}/>
