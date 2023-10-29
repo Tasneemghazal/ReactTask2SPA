973350a32cb7cac942018cd1d5b646c356927d14
Tasneem <tasneemgazal5@gmail.com>
2023-10-29 18:01:59 +0200

    import About.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from './components/footer/Footer.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
 import Products from './components/products/Products.jsx'
+import About from './components/about/About.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Navbar/>
       <Routes>
         <Route path='/products' element={<Products/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
