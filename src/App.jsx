b20c2e9323dcacd014000fc0b6aea4cfb647558d
Tasneem <tasneemgazal5@gmail.com>
2023-10-29 17:39:49 +0200

    import BrowserRouter,Routes,Route in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Footer from './components/footer/Footer.jsx'
 import Navbar from './components/navbar/Navbar.jsx'
 import Products from './components/products/Products.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 export default function App() {
   return (
-    <>
-      <Footer/>
+    <BrowserRouter>
       <Navbar/>
       <Products/>
-    </>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
